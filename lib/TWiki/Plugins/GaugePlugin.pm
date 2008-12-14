2e212fdcdb00b07f7f6808eeb842e1e2e6984668
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2008-12-14 17:49:56 +0000

    Item27: Fix svn properties on all .pm, .tmpl and .txt files
    git-svn-id: http://svn.foswiki.org/trunk/GaugePlugin@1340 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/TWiki/Plugins/GaugePlugin.pm
+++ lib/TWiki/Plugins/GaugePlugin.pm
@@ -38,10 +38,10 @@ use vars qw(
         $defaultTrendWidth $defaultTrendHeight
     );
 
-# This should always be $Rev: 8154 $ so that TWiki can determine the checked-in
+# This should always be $Rev$ so that TWiki can determine the checked-in
 # status of the plugin. It is used by the build automation tools, so
 # you should leave it alone.
-$VERSION = '$Rev: 8154 $';
+$VERSION = '$Rev$';
 
 # This is a free-form string you can use to "name" your own plugin version.
 # It is *not* used by the build automation tools, but is reported as part
